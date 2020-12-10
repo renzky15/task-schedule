518addb29368bf041d44baa53ebcd8d8c4a679b5
renzky15 <renzowensantillan@gmail.com>
2020-12-10 22:44:18 +0800

    comment portion App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css"
 import configStore from "./store/configStore"
 import { login } from "./util/login"
 import { Provider } from "react-redux"
-import { getUser } from "./api/task"
-import { addTask } from "./actions/task"
+// import { getUser } from "./api/task"
+// import { addTask } from "./actions/task"
 import Task from "./components/Task"
 
 const store = configStore()
